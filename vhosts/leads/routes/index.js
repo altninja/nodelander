d17c9091a16fda61d4daeef029bc1e75b06e6234
altninja <benny.gervais@gmail.com>
2016-04-16 15:08:08 +0700

    add buildView function to shared

--- vhosts/leads/routes/index.js
+++ vhosts/leads/routes/index.js
@@ -5,50 +5,51 @@ var router = express.Router();
 
 var general = require('../../shared/config/sample-general.js');
 var social = require('../../shared/config/sample-social.js');
+var view = require('../../shared/helpers/view');
 
-function buildView (req, res, view, title, img) {	
-	var url = req.protocol + '://' + req.get('host') + req.originalUrl;
-	var meta = {
-		title: title,
-		assetbase: general.assetbase.url,
-	    seo_author: social.seo.author,
-	    seo_description: social.seo.description,
-	    seo_canonical: url,
-	    twitter_card: social.twitter.card,
-	    twitter_site: social.twitter.site,
-	    twitter_creator: social.twitter.creator,
-	    twitter_title: title,
-	    twitter_description: social.twitter.description,
-	    twitter_image_src: img,
-	    facebook_og_url: url,
-	    facebook_og_type: social.facebook.og_type,
-	    facebook_og_title: title,
-	    facebook_og_image: img,
-	    facebook_og_description: social.facebook.og_description,
-	    facebook_og_site_name: social.facebook.og_site_name,
-	    google_name: social.google.name,
-	    google_description: social.google.description,
-	    google_image: img
-	}
-	res.render(view, meta);
-}
+// function buildView (req, res, view, title, img) {	
+// 	var url = req.protocol + '://' + req.get('host') + req.originalUrl;
+// 	var meta = {
+// 		title: title,
+// 		assetbase: general.assetbase.url,
+// 	    seo_author: social.seo.author,
+// 	    seo_description: social.seo.description,
+// 	    seo_canonical: url,
+// 	    twitter_card: social.twitter.card,
+// 	    twitter_site: social.twitter.site,
+// 	    twitter_creator: social.twitter.creator,
+// 	    twitter_title: title,
+// 	    twitter_description: social.twitter.description,
+// 	    twitter_image_src: img,
+// 	    facebook_og_url: url,
+// 	    facebook_og_type: social.facebook.og_type,
+// 	    facebook_og_title: title,
+// 	    facebook_og_image: img,
+// 	    facebook_og_description: social.facebook.og_description,
+// 	    facebook_og_site_name: social.facebook.og_site_name,
+// 	    google_name: social.google.name,
+// 	    google_description: social.google.description,
+// 	    google_image: img
+// 	}
+// 	res.render(view, meta);
+// }
 
 router.get('/', function(req, res, next) {
 	var title = "Nodelander | Fast Deploying NodeJS Landing Page and Lead Capture";
 	var img = general.assetbase.url + "images/1.jpg";
-	buildView(req, res, 'index', title, img);
+	view.buildView(req, res, 'index', title, img);
 });
 
 router.get('/a', function(req, res, next) {
 	var title = "A Nodelander | Fast Deploying NodeJS Landing Page and Lead Capture";
 	var img = general.assetbase.url + "images/2.jpg";
-	buildView(req, res, 'index', title, img);
+	view.buildView(req, res, 'index', title, img);
 });
 
 router.get('/b', function(req, res, next) {
 	var title = "B Nodelander | Fast Deploying NodeJS Landing Page and Lead Capture";
 	var img = general.assetbase.url + "images/3.jpg";
-	buildView(req, res, 'index', title, img);
+	view.buildView(req, res, 'index', title, img);
 });
 
 module.exports = router;
---
+++ vhosts/shared/helpers/view.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var general = require('../../shared/config/sample-general.js');
+var social = require('../../shared/config/sample-social.js');
+
+function buildView (req, res, view, title, img) {	
+	var url = req.protocol + '://' + req.get('host') + req.originalUrl;
+	var meta = {
+		title: title,
+		assetbase: general.assetbase.url,
+	    seo_author: social.seo.author,
+	    seo_description: social.seo.description,
+	    seo_canonical: url,
+	    twitter_card: social.twitter.card,
+	    twitter_site: social.twitter.site,
+	    twitter_creator: social.twitter.creator,
+	    twitter_title: title,
+	    twitter_description: social.twitter.description,
+	    twitter_image_src: img,
+	    facebook_og_url: url,
+	    facebook_og_type: social.facebook.og_type,
+	    facebook_og_title: title,
+	    facebook_og_image: img,
+	    facebook_og_description: social.facebook.og_description,
+	    facebook_og_site_name: social.facebook.og_site_name,
+	    google_name: social.google.name,
+	    google_description: social.google.description,
+	    google_image: img
+	}
+	res.render(view, meta);
+}
+
+module.exports = {
+	buildView: buildView
+};
