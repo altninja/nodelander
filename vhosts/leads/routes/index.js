46563d33bdfe81b6a262434533bac50110b744a1
altninja <benny.gervais@gmail.com>
2016-04-16 14:47:02 +0700

    clean up meta config:

--- vhosts/leads/routes/index.js
+++ vhosts/leads/routes/index.js
@@ -6,44 +6,49 @@ var router = express.Router();
 var general = require('../../shared/config/sample-general.js');
 var social = require('../../shared/config/sample-social.js');
 
-var layout = {
-	assetbase: general.assetbase.url,
-    seo_author: social.seo.author,
-    seo_description: social.seo.description,
-    seo_canonical: social.seo.canonical,
-    twitter_card: social.twitter.card,
-    twitter_site: social.twitter.site,
-    twitter_creator: social.twitter.creator,
-    twitter_title: social.twitter.title,
-    twitter_description: social.twitter.description,
-    twitter_image_src: general.assetbase.url + "images/1.jpg",
-    facebook_og_url: social.facebook.og_url,
-    facebook_og_type: social.facebook.og_type,
-    facebook_og_title: social.facebook.og_type,
-    facebook_og_image: general.assetbase.url + "images/1.jpg",
-    facebook_og_description: social.facebook.og_description,
-    facebook_og_site_name: social.facebook.og_site_name,
-    google_name: social.google.name,
-    google_description: social.google.description,
-    google_image: general.assetbase.url + "images/1.jpg"
+function buildView (req, res, view, title, img) {	
+	var url = req.protocol + '://' + req.get('host') + req.originalUrl;
+	var meta = {
+		title: title,
+		assetbase: general.assetbase.url,
+	    seo_author: social.seo.author,
+	    seo_description: social.seo.description,
+	    seo_canonical: url,
+	    twitter_card: social.twitter.card,
+	    twitter_site: social.twitter.site,
+	    twitter_creator: social.twitter.creator,
+	    twitter_title: title,
+	    twitter_description: social.twitter.description,
+	    twitter_image_src: img,
+	    facebook_og_url: url,
+	    facebook_og_type: social.facebook.og_type,
+	    facebook_og_title: title,
+	    facebook_og_image: img,
+	    facebook_og_description: social.facebook.og_description,
+	    facebook_og_site_name: social.facebook.og_site_name,
+	    google_name: social.google.name,
+	    google_description: social.google.description,
+	    google_image: img
+	}
+	res.render(view, meta);
 }
 
 router.get('/', function(req, res, next) {
-	res.render('index', layout);
+	var title = "Nodelander | Fast Deploying NodeJS Landing Page and Lead Capture";
+	var img = general.assetbase.url + "images/1.jpg";
+	buildView(req, res, 'index', title, img);
 });
 
 router.get('/a', function(req, res, next) {
-	res.render('a', {
-        assetbase: general.assetbase.url,
-        social: social
-    });
+	var title = "A Nodelander | Fast Deploying NodeJS Landing Page and Lead Capture";
+	var img = general.assetbase.url + "images/2.jpg";
+	buildView(req, res, 'index', title, img);
 });
 
 router.get('/b', function(req, res, next) {
-	res.render('b', {
-        assetbase: general.assetbase.url,
-        social: social
-    });
+	var title = "B Nodelander | Fast Deploying NodeJS Landing Page and Lead Capture";
+	var img = general.assetbase.url + "images/3.jpg";
+	buildView(req, res, 'index', title, img);
 });
 
 module.exports = router;
